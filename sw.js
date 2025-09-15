8c87e4f6e0d7463616884203652e9e15afb5fcd3
MD SHARIFUL ISLAM <94898754+msharifulvisionary@users.noreply.github.com>
2025-09-15 14:17:22 +0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "physics-app-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html"
+];
+
+// Install Service Worker
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch from cache or network
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
