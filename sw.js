c598957f51879b86986d068baaf18303d318a4ae
MD SHARIFUL ISLAM <94898754+msharifulvisionary@users.noreply.github.com>
2025-09-22 16:59:26 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,42 +1,21 @@
-const CACHE_NAME = "physics-app-cache-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/sw.js"
-];
-
-// Install Service Worker and cache files
-self.addEventListener("install", event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log("Caching files...");
-      return cache.addAll(urlsToCache);
+    caches.open('physics-web-cache').then((cache) => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        'https://i.imgur.com/bLsJS9s.png',
+        'https://i.imgur.com/oSvcwIl.png'
+      ]);
     })
   );
 });
 
-// Serve files from cache, fallback to network
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
-    })
-  );
-});
-
-// Activate and clean old cache
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            console.log("Old cache removed:", cache);
-            return caches.delete(cache);
-          }
-        })
-      );
     })
   );
 });
